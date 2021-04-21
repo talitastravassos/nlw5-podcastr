f8ddd42a840176bb0f9f364fe3ed8c04c06f10e0
talitast <talita.travassos@projuris.com.br>
2021-04-21 17:14:00 -0300

    add Player component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
+import Header from "../components/Header";
+import Player from "../components/Player";
+import styles from "../styles/app.module.scss";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Component {...pageProps} />
+        <Header />
+      </main>
+      <Player />
+    </div>
+  );
 }
 
 export default MyApp;
